a11b2e644d44fb3fd63e3dab99b312660f7a6618
AlessandroOS <alessandro0014@hotmail.com>
2024-06-25 22:58:12 -0300

    validaçao do controler cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
+import * as yup from 'yup';
 
 interface Icidade {
     nome:string;
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const create = (req: Request<{}, {}, Icidade>, res: Response) => {
+const bodyValidation: yup.ObjectSchema<Icidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
 
-    console.log(req.body);
+});
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
+
+    let validatedData: Icidade | undefined;
+
+    try{
+        validatedData = await bodyValidation.validate(req.body);
+    }catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.json ({
+            errors: {
+                default: yupError.message,
+            }
+        })
+    }
+
+    console.log(validatedData);
 
     return res.send('Create!');
 };
